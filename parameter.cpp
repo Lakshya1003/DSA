5dcce89691133ffd276af255b6fcb9bb6508aba7
Lakshya Raj Malviya <malviyalakshyaraj@gmail.com>
2024-12-27 08:29:54 +0000

    just depth one

--- parameter.cpp
+++ parameter.cpp
@@ -2,11 +2,54 @@
 // pass by value 
 // pass by reference 
 // pass by address 
+// ===
+// ===
+// ===
+// ===
+// ===
+// ===
+// ===
+// ===
+// here this example is for pass by value
+int swap(int x, int y ){
+    int temp;
+    temp = x;
+    x= y;
+    y = temp;
+}
+#include<iostream>
+using namespace std;
+int main()
+{
+int a = 10;
+int b = 30;
+swap(a,b);
+cout<<a<<endl<<b;
+return 0;
+}
+// ============ here is the example of pass by reference
 int swap(int &x, int &y ){
     int temp;
     temp = x;
     x= y;
     y = temp;
+}
+#include<iostream>
+using namespace std;
+int main()
+{
+int a = 10;
+int b = 30;
+swap(a,b);
+cout<<a<<endl<<b;
+return 0;
+}
+// here is the example of pass by address 
+int swap(int *x, int *y ){
+    int temp;
+    temp = *x;
+    *x= *y;
+    *y = temp;
 }
 #include<iostream>
 using namespace std;
