37529d157623b7374a6051e7ac3c042c3e0568ea
Lakshya Raj Malviya <malviyalakshyaraj@gmail.com>
2026-06-21 18:13:16 +0530

    Time: 58 ms (18.45%) | Memory: 153.3 MB (14.10%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int h = 0;
+        int l = 0;
+        vector<int> ans;
+        priority_queue<pair<int, int>> pq;
+
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push({nums[i], i});
+
+            if (i >= k - 1) {
+
+                while (!pq.empty() && pq.top().second <= i - k) {
+                    pq.pop();
+                }
+
+                ans.push_back(pq.top().first);
+            }
+        }
+        return ans;
+    }
+};
