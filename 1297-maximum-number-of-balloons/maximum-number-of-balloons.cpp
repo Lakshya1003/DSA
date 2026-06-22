b342bfc4f6ab1999f5e467518b4d24ee7fc045f1
Lakshya Raj Malviya <malviyalakshyaraj@gmail.com>
2026-06-22 10:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (39.61%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -2,18 +2,12 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         if(text.length()<7) return 0;
-        sort(text.begin() , text.end());
         unordered_map<char , int> cf;
         for(char i : text){
             if(i == 'b' || i == 'l' || i == 'o' || i == 'a' || i == 'n') cf[i]++;
         }
         if(cf.empty()) return 0;
-        int p = 0;
-        p = cf['b'];
-        p = min(p , cf['a']);
-        p = min(p , cf['l'] / 2);
-        p = min(p , cf['o']/2);
-        p = min(p , cf['n']);
+        int p = min({cf['a'] , cf['b'] , cf['l'] / 2 , cf['o']/2 , cf['n']  });
         return p;
     }
 };
