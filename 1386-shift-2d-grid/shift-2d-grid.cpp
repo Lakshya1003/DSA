5563121ce74d4ce48dcdd7d384afda508d74be89
Lakshya Raj Malviya <malviyalakshyaraj@gmail.com>
2026-07-20 13:05:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (14.15%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        vector<int> v;
+        for(int i = 0 ; i < grid.size() ; i++){
+            for(int j = 0 ; j < grid[0].size() ; j++){
+                v.push_back(grid[i][j]);
+            }
+        }
+        k %= v.size();
+        reverse(v.begin() , v.end());
+        reverse(v.begin() , v.begin() + k);
+        reverse(v.begin()+k , v.end());
+
+        
+        int d = 0;
+        for(int i = 0 ; i < grid.size() ; i++){
+            for(int j = 0 ; j < grid[0].size() ; j++){
+                int a  = v[d++];
+                grid[i][j] = a;
+            }
+        }
+        return grid;
+    }
+};
